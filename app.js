6f1e035eb9493279658685d6c67187d6af606c85
Eduardo P <edtvports@gmail.com>
2023-02-10 12:17:55 -0300

    Working positive numbers calculator

--- app.js
+++ app.js
@@ -1,56 +1,155 @@
 var resultHtml = document.getElementById("result");
-var firstValue, secondValue, resultValue;
-var tempValue = "";
-var operation = "";
-var keep = false;
-var displayValue = "";
+var fullExpression = "0";
+function clearDisplayButton() {
+    fullExpression = "0";
+    updateHtml();
+}
 function readInputButton(valueInput) {
-    tempValue = tempValue + valueInput;
-    displayValue = (displayValue == "0" || displayValue == "Infinity" || displayValue == "NaN") ? valueInput : displayValue + valueInput;
-    updateResult();
+    console.log("pressed: " + valueInput);
+    if (valueInput == ')' && !fullExpression.includes('('))
+        return;
+    if (valueInput == '(' && isNumeric(fullExpression[fullExpression.length - 1]))
+        fullExpression += '*';
+    if (fullExpression == "0" || fullExpression == "Error")
+        fullExpression = valueInput;
+    else
+        fullExpression += valueInput;
+    multiplyNumberAfterParenthesis();
+    updateHtml();
+    console.log("inserted Expre: " + fullExpression);
 }
 function readOperationButton(operationInput) {
-    if (operation == "") {
-        if (tempValue != "") {
-            firstValue = parseFloat(tempValue);
-        }
-        tempValue = "";
-        displayValue = displayValue + operationInput;
-        operation = operationInput;
-        updateResult();
-    }
+    console.log("pressed: " + operationInput);
+    if (!isNumeric(fullExpression[fullExpression.length - 1]))
+        return;
+    fullExpression += operationInput;
+    updateHtml();
+    console.log("inserted Expre: " + fullExpression);
 }
 function calculateResult() {
-    secondValue = parseFloat(tempValue);
-    tempValue = "";
-    switch (operation) {
-        case '/':
-            resultValue = firstValue / secondValue;
-            break;
-        case '*':
-            resultValue = firstValue * secondValue;
-            break;
-        case '+':
-            resultValue = firstValue + secondValue;
-            break;
-        case '-':
-            resultValue = firstValue - secondValue;
-            break;
+    removeUselessOperations();
+    while (howManyParenthesisOpen() > 0)
+        fullExpression += ')';
+    fullExpression = evaluateExpression(fullExpression);
+    console.log("final Expre: " + fullExpression);
+    updateHtml();
+}
+function evaluateExpression(expression) {
+    console.log("expression to calc: " + expression);
+    if (!expression.includes('+') && !expression.includes('-') && !expression.includes('*') && !expression.includes('/') && !expression.includes('(') && !expression.includes(')'))
+        return expression;
+    while (expression.includes('(') && expression.includes(')')) {
+        var startIndex = 0, endIndex = 0;
+        console.log("expression to calc: " + expression);
+        for (var index = 0; index < expression.length; index++) {
+            if (expression[index] == '(')
+                startIndex = index;
+            if (expression[index] == ')')
+                endIndex = index;
+            if (endIndex != 0 && endIndex > startIndex)
+                break;
+        }
+        var subExpressionResult = evaluateExpression(expression.slice(startIndex + 1, endIndex));
+        expression = expression.slice(0, startIndex) + subExpressionResult + expression.slice(endIndex + 1, expression.length);
     }
-    keep = true;
-    firstValue = resultValue;
-    displayValue = resultValue.toString();
-    operation = "";
-    updateResult();
+    while (expression.includes('*') || expression.includes('/')) {
+        var firstIndexOne = 0, finalIndexOne = 0, firstIndexTwo = 0, finalIndexTwo = 0;
+        var firstNumber = "", secondNumber = "";
+        for (var char = 0; char < expression.length; char++) {
+            if (!isNumeric(expression[char - 1]) || char == 0)
+                firstIndexOne = char;
+            if (expression[char] == '*') {
+                finalIndexOne = char;
+                firstNumber = expression.slice(firstIndexOne, finalIndexOne);
+                firstIndexTwo = char + 1;
+                for (var forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        finalIndexTwo = forwardChar;
+                        secondNumber = expression.slice(firstIndexTwo, finalIndexTwo);
+                        var result = Number(firstNumber) * Number(secondNumber);
+                        expression = expression.slice(0, firstIndexOne) + result.toString() + expression.slice(finalIndexTwo + 1, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+            if (expression[char] == '/') {
+                finalIndexOne = char;
+                firstNumber = expression.slice(firstIndexOne, finalIndexOne);
+                firstIndexTwo = char + 1;
+                for (var forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        finalIndexTwo = forwardChar;
+                        secondNumber = expression.slice(firstIndexTwo, finalIndexTwo);
+                        if (Number(secondNumber) == 0)
+                            return "Error";
+                        var result = Number(firstNumber) / Number(secondNumber);
+                        expression = expression.slice(0, firstIndexOne) + result.toString() + expression.slice(finalIndexTwo + 1, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    while (expression.includes('+') || expression.includes('-')) {
+        var firstNumber = "", secondNumber = "";
+        for (var char = 0; char < expression.length; char++) {
+            if (expression[char] == '+') {
+                for (var forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (isNumeric(expression[forwardChar]))
+                        secondNumber += expression[forwardChar];
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        var result = Number(firstNumber) + Number(secondNumber);
+                        expression = (forwardChar == expression.length - 1) ? result.toString() : result.toString() + expression.slice(forwardChar, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+            else if (expression[char] == '-') {
+                for (var forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (isNumeric(expression[forwardChar]))
+                        secondNumber += expression[forwardChar];
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        var result = Number(firstNumber) + (Number(secondNumber) * -1);
+                        expression = (forwardChar != expression.length - 1) ? result.toString() + expression.slice(forwardChar, expression.length) : result.toString();
+                        break;
+                    }
+                }
+                break;
+            }
+            firstNumber += expression[char];
+        }
+    }
+    console.log("result " + expression);
+    return expression;
 }
-function clearDisplayButton() {
-    displayValue = "0";
-    firstValue = 0;
-    secondValue = 0;
-    tempValue = "";
-    operation = "";
-    updateResult();
+function removeUselessOperations() {
+    for (var char = 0; char < fullExpression.length; char++)
+        if (!isNumeric(fullExpression[char]) && (fullExpression[char] != '(' && fullExpression[char] != ')') && (!isNumeric(fullExpression[char + 1]) && (fullExpression[char + 1] != '(' && fullExpression[char + 1] != ')')))
+            fullExpression = fullExpression.slice(0, char) + fullExpression.slice(char + 1, fullExpression.length);
 }
-function updateResult() {
-    resultHtml.innerHTML = displayValue;
+function multiplyNumberAfterParenthesis() {
+    for (var char = 0; char < fullExpression.length; char++)
+        if (fullExpression[char] == ')' && isNumeric(fullExpression[char + 1]))
+            fullExpression = fullExpression.slice(0, char + 1) + '*' + fullExpression.slice(char + 1, fullExpression.length);
+}
+function howManyParenthesisOpen() {
+    if (!fullExpression.includes('('))
+        return 0;
+    var openParenthesis = 0;
+    for (var char = 0; char < fullExpression.length; char++) {
+        if (fullExpression[char] == '(')
+            openParenthesis++;
+        if (fullExpression[char] == ')')
+            openParenthesis--;
+    }
+    return openParenthesis;
+}
+function isNumeric(char) {
+    return !isNaN(Number(char));
+}
+function updateHtml() {
+    resultHtml.textContent = fullExpression;
 }
--- app.ts
+++ app.ts
@@ -1,69 +1,189 @@
 const resultHtml = document.getElementById("result");
 
-let firstValue: number, secondValue: number, resultValue: number;
-let tempValue = "";
-let operation = "";
 
-let keep = false;
-let displayValue = "";
+let fullExpression: string = "0";
 
+function clearDisplayButton(): void {
+    fullExpression = "0";
+    updateHtml();
+}
 
 function readInputButton(valueInput: string): void {
-    tempValue = tempValue + valueInput;
-    displayValue = (displayValue == "0" || displayValue == "Infinity" || displayValue == "NaN") ? valueInput : displayValue + valueInput;
-    updateResult();
+    console.log("pressed: "+valueInput)
+
+    if (valueInput == ')' && !fullExpression.includes('('))
+        return;
+    if (valueInput == '(' && isNumeric(fullExpression[fullExpression.length - 1]))
+        fullExpression += '*';
+
+
+    if (fullExpression == "0" || fullExpression == "Error")
+        fullExpression = valueInput;
+    else
+        fullExpression += valueInput;
+    multiplyNumberAfterParenthesis();
+
+    updateHtml();
+    console.log("inserted Expre: "+fullExpression);
+
 }
 
 function readOperationButton(operationInput: string): void {
-    if (operation == "") {
-        if (tempValue != "") {
-            firstValue = parseFloat(tempValue);
+    console.log("pressed: "+operationInput)
+
+    if (!isNumeric(fullExpression[fullExpression.length - 1]))
+        return;
+
+    fullExpression += operationInput;
+    updateHtml();
+    console.log("inserted Expre: "+fullExpression);
+
+}
+
+function calculateResult() {
+    removeUselessOperations();
+    while (howManyParenthesisOpen() > 0)
+        fullExpression += ')';
+    fullExpression = evaluateExpression(fullExpression);
+    console.log("final Expre: "+fullExpression);
+    updateHtml();
+}
+
+function evaluateExpression(expression: string): string {
+
+    console.log("expression to calc: " + expression);
+    if (!expression.includes('+') && !expression.includes('-') && !expression.includes('*') && !expression.includes('/') && !expression.includes('(') && !expression.includes(')'))
+        return expression;
+
+    while (expression.includes('(') && expression.includes(')')) {
+        let startIndex = 0, endIndex = 0;
+        console.log("expression to calc: " + expression);
+
+        for (let index = 0; index < expression.length; index++) {
+            if (expression[index] == '(')
+                startIndex = index;
+            if (expression[index] == ')')
+                endIndex = index;
+            if (endIndex != 0 && endIndex > startIndex)
+                break;
         }
 
-        tempValue = "";
-        
-        displayValue = displayValue + operationInput;
-        operation = operationInput;
-        updateResult();
+        let subExpressionResult = evaluateExpression(expression.slice(startIndex + 1, endIndex));
+        expression = expression.slice(0, startIndex) + subExpressionResult + expression.slice(endIndex + 1, expression.length);
     }
-}
 
-function calculateResult(): void {
-    secondValue = parseFloat(tempValue);
-    tempValue = "";
+    while (expression.includes('*') || expression.includes('/')) {
+        let firstIndexOne = 0, finalIndexOne = 0,
+            firstIndexTwo = 0, finalIndexTwo = 0;
+        let firstNumber = "", secondNumber = "";
 
-    switch (operation) {
-        case '/':
-            resultValue = firstValue / secondValue;
-            break;
-        case '*':
-            resultValue = firstValue * secondValue;
-            break;
-        case '+':
-            resultValue = firstValue + secondValue;
-            break;
-        case '-':
-            resultValue = firstValue - secondValue;
-            break;
+        for (let char = 0; char < expression.length; char++) {
+            if (!isNumeric(expression[char - 1]) || char == 0)
+                firstIndexOne = char;
+
+            if (expression[char] == '*') {
+                finalIndexOne = char;
+                firstNumber = expression.slice(firstIndexOne, finalIndexOne);
+                firstIndexTwo = char + 1;
+                for (let forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        finalIndexTwo = forwardChar;
+                        secondNumber = expression.slice(firstIndexTwo, finalIndexTwo);
+                        let result: number = Number(firstNumber) * Number(secondNumber);
+                        expression = expression.slice(0, firstIndexOne) + result.toString() + expression.slice(finalIndexTwo + 1, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+            if (expression[char] == '/') {
+                finalIndexOne = char;
+                firstNumber = expression.slice(firstIndexOne, finalIndexOne);
+                firstIndexTwo = char + 1;
+                for (let forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        finalIndexTwo = forwardChar;
+                        secondNumber = expression.slice(firstIndexTwo, finalIndexTwo);
+                        if (Number(secondNumber) == 0)
+                            return "Error";
+                        let result: number = Number(firstNumber) / Number(secondNumber);
+                        expression = expression.slice(0, firstIndexOne) + result.toString() + expression.slice(finalIndexTwo + 1, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
-    keep = true;
 
-    firstValue = resultValue;
-    displayValue = resultValue.toString();
-    operation = "";
-    updateResult();
+    while (expression.includes('+') || expression.includes('-')) {
+        let firstNumber = "", secondNumber = "";
+        for (let char = 0; char < expression.length; char++) {
+            if (expression[char] == '+') {
+                for (let forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (isNumeric(expression[forwardChar]))
+                        secondNumber += expression[forwardChar];
 
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        let result: number = Number(firstNumber) + Number(secondNumber);
+                        expression = (forwardChar == expression.length - 1) ? result.toString() : result.toString() + expression.slice(forwardChar, expression.length);
+                        break;
+                    }
+                }
+                break;
+            }
+
+            else if (expression[char] == '-') {
+                for (let forwardChar = char + 1; forwardChar < expression.length; forwardChar++) {
+                    if (isNumeric(expression[forwardChar]))
+                        secondNumber += expression[forwardChar];
+
+                    if (!isNumeric(expression[forwardChar]) || forwardChar == expression.length - 1) {
+                        let result: number = Number(firstNumber) + (Number(secondNumber) * -1);
+                        expression = (forwardChar != expression.length - 1) ? result.toString() + expression.slice(forwardChar, expression.length) : result.toString();
+                        break;
+                    }
+                }
+                break;
+            }
+
+            firstNumber += expression[char];
+        }
+    }
+
+    console.log("result "+expression);
+    return expression;
 }
 
-function clearDisplayButton(): void {
-    displayValue = "0";
-    firstValue = 0;
-    secondValue = 0;
-    tempValue = "";
-    operation = "";
-    updateResult();
+function removeUselessOperations() {
+    for (let char = 0; char < fullExpression.length; char++)
+        if (!isNumeric(fullExpression[char]) && (fullExpression[char] != '(' &&  fullExpression[char] != ')') && (!isNumeric(fullExpression[char + 1]) && (fullExpression[char + 1] != '(' &&  fullExpression[char + 1] != ')')))
+                fullExpression = fullExpression.slice(0, char) + fullExpression.slice(char + 1, fullExpression.length);
 }
 
-function updateResult(): void {
-    resultHtml!.innerHTML = displayValue;
+function multiplyNumberAfterParenthesis() {
+    for (let char = 0; char < fullExpression.length; char++)
+        if (fullExpression[char] == ')' && isNumeric(fullExpression[char + 1]))
+            fullExpression = fullExpression.slice(0, char + 1) + '*' + fullExpression.slice(char + 1, fullExpression.length);
+}
+
+function howManyParenthesisOpen(): number {
+    if (!fullExpression.includes('('))
+        return 0;
+    let openParenthesis = 0;
+    for (let char = 0; char < fullExpression.length; char++) {
+        if (fullExpression[char] == '(')
+            openParenthesis++;
+        if (fullExpression[char] == ')')
+            openParenthesis--;
+    }
+    return openParenthesis;
+}
+
+function isNumeric(char: string){
+    return !isNaN(Number(char));
+}
+
+function updateHtml(): void {
+    resultHtml!.textContent = fullExpression;
 }
